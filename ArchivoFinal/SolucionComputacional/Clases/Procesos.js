f6b8bbd92d9288e8422a04492cfa338f9ca445b0
Joseph <j-vv01@hotmail.com>
2015-06-04 00:17:08 -0600

    Creacion del interfaz del Proyecto

---
+++ ArchivoFinal/SolucionComputacional/Clases/Procesos.js
@@ -0,0 +1,17 @@
+function Redireccionar(tipo) {
+
+    var valor = document.getElementById("Nombre").value;
+    alert(valor)
+    var direccion = ("LibretaViajero.html?json=" + tipo);
+
+
+    if (tipo == "old") {
+        location.href = direccion + "&user=NONE";
+    } else {
+        if (!valor == "") {
+            location.href = direccion + "&user=" + valor;
+        } else {
+            alert("No debe dejar el espacio en blanco");
+        }
+    }
+}
