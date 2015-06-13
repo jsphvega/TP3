a9ccba64e1844ca600ea73b7d5655012f022098c
Ruben <malusoce96@gmail.com>
2015-06-13 11:11:26 -0600

    final project

--- ArchivoFinal/SolucionComputacional/ArchivosJs/ejemplos.js
+++ ArchivoFinal/SolucionComputacional/ArchivosJs/ejemplos.js
@@ -49,37 +49,37 @@ setTimeout(
 
     var places = [
       [
-        "Moravia, San José, Costa Rica",
-        18.934150289348562,
-        -71.09717799999999,
-        "#primer #viaje",
-        "01/06/2015",
-        "06/06/2015",
-        "#Pinto",
-        "#Joseph #Lucia #Yulay"
-      ],
-      [
-        "Tokio, Japón",
-        139.69170639999993,
-        35.6894875,
-        "#segundo #viaje",
-        "06/06/2015",
-        "08/06/2015",
-        "#sushi",
-        "#Joseph #Lucia #Yulay"
-      ],
-      [
         "Madrid, España",
-        -3.7037901999999576,
-        40.4167754,
+        37.772323,
+        -122.214897,
         "#tercer #viaje",
         "08/06/2015",
         "11/06/2015",
         "#CorderoAsado",
         "#Joseph #Lucia #Yulay"
+      ],[
+        "Moravia, San José, Costa Rica",
+        21.291982,
+        -157.821856,
+        "#dfhxdxgh ",
+        "2015-06-13",
+        "2015-06-13",
+        "#fhthsx ",
+        "#aegadgzz "
+      ],[
+        "Tokio, Japón",
+        -157.821856,
+        178.431,
+        "#dfhxdxgh ",
+        "2015-06-13",
+        "2015-06-13",
+        "#fhthsx ",
+        "#aegadgzz "
       ]
     ];
 
-    googleMapsAdmin.loadPlaces(places);
+    //googleMapsAdmin.loadPlaces(places);
+    //googleMapsAdmin.showPath();
+
   }
   , 5000);
